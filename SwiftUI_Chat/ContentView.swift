8251068c4de763404c3261404681f9867fe5d4fc
Albert <ajcatano@gmail.com>
2022-07-07 23:10:01 -0400

    Initial Commit

---
+++ SwiftUI_Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Chat
+//
+//  Created by Warkois on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Chat/SwiftUI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatApp.swift
+//  SwiftUI_Chat
+//
+//  Created by Warkois on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
